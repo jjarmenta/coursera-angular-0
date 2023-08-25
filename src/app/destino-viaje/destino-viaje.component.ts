9ae2883f5491f4fbb9203e3c55dce6e5cd194fdc
Jose Jorge Armenta Martínez <jarmenta@merqueo.com>
2023-08-25 00:51:33 -0500

    Add directives

--- src/app/destino-viaje/destino-viaje.component.ts
+++ src/app/destino-viaje/destino-viaje.component.ts
@@ -9,11 +9,13 @@ import { DestinoViaje } from "../models/destino-viaje.model";
 export class DestinoViajeComponent {
   @HostBinding('attr.class') cssClass = 'col-md-4';
   @Input() destination: DestinoViaje;
+  @Input() position: number;
   @Output() clicked: EventEmitter<DestinoViaje>;
 
   constructor() {
     this.destination = new DestinoViaje('', '');
     this.clicked = new EventEmitter();
+    this.position = 0;
   }
 
   favorite() {
--- src/app/models/destino-viaje.model.ts
+++ src/app/models/destino-viaje.model.ts
@@ -1,7 +1,9 @@
 export class DestinoViaje {
   private selected: boolean;
+  public services: string[];
   constructor(public name: string, public url: string) {
     this.selected = false;
+    this.services = ['Piscina', 'Desayuno incluido'];
   }
 
   isSelected(): boolean {
